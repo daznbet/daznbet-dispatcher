c9612f90737c2795fc0ea176403db61203caa761
Neemias Junior <neemiasjnr@mac-home.local>
2019-03-31 01:09:26 -0300

    event log working

---
+++ data.js
@@ -0,0 +1,270 @@
+module.exports = [
+  {
+    time: '1:37:10',
+    payload: {
+      message: 'startGame'
+    }
+  },
+  {
+    time: '1:37:15',
+    payload: {
+      message: 'pass',
+      team: 'Empoli',
+      player: 5
+    }
+  },
+  {
+    time: '1:37:19',
+    payload: {
+      message: 'pass',
+      team: 'Empoli',
+      player: 22
+    }
+  },
+  {
+    time: '1:37:22',
+    payload: {
+      message: 'pass',
+      team: 'Empoli',
+      player: 3
+    }
+  },
+  {
+    time: '1:37:28',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 20
+    }
+  },
+  {
+    time: '1:37:30',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 30
+    }
+  },
+  {
+    time: '1:37:37',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 23
+    }
+  },
+  {
+    time: '1:37:41',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 37
+    }
+  },
+  {
+    time: '1:37:46',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 33
+    }
+  },
+  {
+    time: '1:37:51',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 20
+    }
+  },
+  {
+    time: '1:37:53',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 17
+    }
+  },
+  {
+    time: '1:37:56',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 5
+    }
+  },
+  {
+    time: '1:38:03',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 3
+    }
+  },
+  {
+    time: '1:38:16',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 37
+    }
+  },
+  {
+    time: '1:38:19',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 33
+    }
+  },
+  {
+    time: '1:38:20',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 30
+    }
+  },
+  {
+    time: '1:38:25',
+    payload: {
+      message: 'pass',
+      team: 'Empoli',
+      player: 9
+    }
+  },
+  {
+    time: '1:38:27',
+    payload: {
+      message: 'pass',
+      team: 'Empoli',
+      player: 7
+    }
+  },
+  {
+    time: '1:38:39',
+    payload: {
+      message: 'shotOnTarget',
+      team: 'Juventus',
+      player: 20
+    }
+  },
+  {
+    time: '1:38:50',
+    payload: {
+      message: 'pass',
+      team: 'Empoli',
+      player: 69
+    }
+  },
+  {
+    time: '1:38:53',
+    payload: {
+      message: 'pass',
+      team: 'Empoli',
+      player: 10
+    }
+  },
+  {
+    time: '1:38:55',
+    payload: {
+      message: 'pass',
+      team: 'Empoli',
+      player: 6
+    }
+  },
+  {
+    time: '1:38:57',
+    payload: {
+      message: 'pass',
+      team: 'Empoli',
+      player: 10
+    }
+  },
+  {
+    time: '1:39:00',
+    payload: {
+      message: 'pass',
+      team: 'Empoli',
+      player: 22
+    }
+  },
+  {
+    time: '1:39:02',
+    payload: {
+      message: 'pass',
+      team: 'Empoli',
+      player: 5
+    }
+  },
+  {
+    time: '1:39:15',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 69
+    }
+  },
+  {
+    time: '1:39:18',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 3
+    }
+  },
+  {
+    time: '1:39:20',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 33
+    }
+  },
+  {
+    time: '1:39:23',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 3
+    }
+  },
+  {
+    time: '1:39:25',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 30
+    }
+  },
+  {
+    time: '1:39:28',
+    payload: {
+      message: 'pass',
+      team: 'Juventus',
+      player: 3
+    }
+  },
+  {
+    time: '1:39:30',
+    payload: {
+      message: 'goalPass',
+      team: 'Juventus',
+      player: 17
+    }
+  },
+  {
+    time: '1:39:31',
+    payload: {
+      message: 'goal',
+      team: 'Juventus',
+      player: 18
+    }
+  },
+  {
+    time: '1:39:42',
+    payload: {
+      message: 'endGame'
+    }
+  }
+]
--- dispatcher.js
+++ dispatcher.js
@@ -1,211 +1,34 @@
 const uuid = require('uuid')
 const Producer = require('sqs-producer')
+const moment = require('moment')
+const events = require('./data')
 
-const events = [
-  { time: 1, payload: { message: '1' } },
-  { time: 4, payload: { message: '4' } },
-  { time: 5, payload: { message: '5' } },
-  { time: 7, payload: { message: '7' } },
-  { time: 14, payload: { message: '14' } },
-]
-
-const realEvents = [
-  {
-    time: '1:37:17,
-    message: 'startGame',
-  },
-  {
-    time: '1:37:20',
-    message: 'pass'
-    team: 'Empoli'
-    player: 22
-  },
-  {
-    time: '1:37:22',
-    message: 'pass'
-    team: 'Empoli',
-    player: 3
-  },
-  {
-    time: '1:37:29',
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:37:38',
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:37:42',
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:37:46
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:37:51'
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:37:53'
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:37:57'
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:38:16'
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:38:19'
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:38:20'
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:38:25'
-    message: 'pass'
-    team: 'Empoli',
-    player: ''
-  },
-  {
-    time: '1:38:27'
-    message: 'pass'
-    team: 'Empoli',
-    player: ''
-  },
-  {
-    time: '1:38:32'
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:38:38'
-    message: 'shotOnTarget'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:38:53'
-    message: 'pass'
-    team: 'Empoli',
-    player: ''
-  },
-  {
-    time: '1:38:55'
-    message: 'pass'
-    team: 'Empoli',
-    player: ''
-  },
-  {
-    time: '1:38:57'
-    message: 'pass'
-    team: 'Empoli',
-    player: ''
-  },
-  {
-    time: '1:39:00'
-    message: 'pass'
-    team: 'Empoli',
-    player: ''
-  },
-  {
-    time: '1:39:02'
-    message: 'pass'
-    team: 'Empoli',
-    player: ''
-  },
-  {
-    time: '1:39:15'
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:39:18'
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:39:20'
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:39:23'
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:39:25'
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:39:29'
-    message: 'pass'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:39:31'
-    message: 'goal'
-    team: 'Juventus',
-    player: ''
-  },
-  {
-    time: '1:39:33'
-    message: 'endGame'
-    team: 'Juventus',
-    player: ''
-  },
-]
 
 const producer = Producer.create({
   queueUrl: `https://sqs.us-east-1.amazonaws.com/374047294805/daznbet-events`,
   region: 'us-east-1'
 })
 
+const getTime = (strHour) =>  moment(strHour, 'h:mm:ss')
+
+const calculateDiff = (startTime) => (time) => moment.duration(time.diff(startTime.diff(1000))).asSeconds()
+
 const dispatcher = (events) => {
   const epoch = Date.now()
   let lastTime = 0
+  const start = getTime(events[0].time)
+  const diffFromStart = calculateDiff(start)
 
   setInterval(() => {
     const messages = events
-      .filter(e => e.time >= lastTime && e.time < (Date.now() - epoch) / 1000)
+      .filter(e => diffFromStart(getTime(e.time)) >= lastTime && diffFromStart(getTime(e.time)) < (Date.now() - epoch) / 1000)
       .map(e => ({ id: uuid(), body: JSON.stringify(e.payload) }))
+
     if (messages && messages.length !== 0) {
+      console.log(messages)
       producer.send(messages, () => { })
     }
+
     lastTime = (Date.now() - epoch) / 1000
   }, 1000);
 }
